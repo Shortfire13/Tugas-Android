d6c91824656fdb35f3ed556b4e75c5af4128638a
Hanif Rimamtomo <65529407+Shortfire13@users.noreply.github.com>
2021-04-20 20:28:57 +0700

    Minggu6-Tugas2

---
+++ Minggu6_2/app/src/main/java/com/example/minggu6_2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.minggu6_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonImplicit (View view){
+        Intent intent = new Intent(MainActivity.this, ImpIntentApp.class);
+        startActivity(intent);
+    }
+
+    public void buttonExplicit (View view){
+        Intent intent = new Intent(MainActivity.this, ExpIntentApp.class);
+        startActivity(intent);
+    }
+}
